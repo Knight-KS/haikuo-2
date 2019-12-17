8d123e950a7760e5d5f928d9f6d94d5909fae8da
tianshideyou <chenhua.xu@qq.com>
2019-12-17 12:34:13 +0800

    海阔视界的解析js代码

---
+++ global_Three.js
@@ -0,0 +1,653 @@
+//==========via-plugin:6==========
+//==========百度文库直接下载==========
+(function() {
+
+(function(){
+window.vip=[
+
+   "腾讯专用@https://vip.66parse.club/?url=",
+    "爱奇艺专用@http://ppplay.cn/jiexi.html?url=",
+ "优酷专用@http://jx.618g.com/?url=", 
+     "360解析@http://jx.cx77m1.cn/?url=",   
+    "初心解析@http://jx.bwcxy.com/?v=",
+    "1907@https://z1.m1907.cn/?jx=",
+     "云播放@http://yun.mt2t.com/lines?url=", 
+    "360dy接口@http://yun.360dy.wang/?url=",
+    "科技解析@http://ka61b.cn/jx.php?url=",
+
+     "秒播解析@http://jx.yuanzhi668.cn/?url=",
+    "豪华啦@http://api.lhh.la/vip/?url=",
+    "玩得嗨@http://tv.wandhi.com/go.html?url=",
+    "1ff1解析@http://jx.1ff1.cn/?url=",
+    "17K云@http://17kyun.com/api.php?url=",
+    "购买的解析@http://jx.vipshares.xyz/index1.php?url=",
+    "调试@https://vip.mpos.ren/v/?url=",
+    "黑米解析@https://www.heimijx.com/jx/api/?url=",
+    "55解析@http://55jx.top/?url=",
+    "517解析@http://cn.bjbanshan.cn/jx.php?url=",
+    "凉城解析@http://jx.mw0.cc/?url=",
+    "爸比解析@http://www.33tn.cn/?url=",
+    "星空解析@https://jx.fo97.cn/?url=",
+    "660e@https://660e.com/?url=",
+    "狂野解析@https://api.653520.top/vip/?url=",
+    "冰豆解析@http://api.bingdou.net/?url=",
+    "金桥解析@http://jqaaa.com/jx.php?url=",
+    "船长解析@http://czjx8.com/?url=",
+    "19解析@http://19g.top/?url=",
+    "OK解析@http://okjx.cc/?url=",
+    "618G@https://jx.618g.com/?url=",
+    "瑞特解析@http://jx.0421v.pw/index.php?url=",
+    "蓝科迪@http://api.lkdmkj.com/jx/jx00/index.php?url=" ,    
+    "搜搜库1@http://www.guandianzhiku.com/v/s/?url=",
+    "未知@https://vip.jlsprh.com/?url=",
+    "bofang@http://bofang.online/?url=",
+    "play1@http://play1.online/?url=",
+    "ckplay@http://ckplay.online/?url=",
+    "52088@http://52088.online/?url=",
+    "69p@http://69p.top/?url=",
+    "猪蹄无广告@http://jx.iztyy.com/svip/?url=",
+    "猪蹄无广告1@https://jx.iztyy.com/svip/?url=",
+    "蚂蚁智能解析@http://jx.v5ant.com/?v=",
+    "test解析@http://www.now10.top/?url=",
+    "百度API@http://app.baiduoos.cn:2019/vip/?url=",
+    "易云解析@http://v.ioto.vip/?v=",
+    "Play99解析@http://www.play99.top/?url=",
+    "天天@http://jx.mba98.com/?url=",
+    "音萌云@https://api.v6.chat/?url=",
+    "解析视频@http://jx.xzsdwl.cn/?url=",
+    "4080@http://www.4080.xyz/jx/?url=",
+    "花园影视@http://j.zz22x.com/jx/?url=",
+    "大白影视@http://jx.myzch.cn/jx/?v=",
+    "97解析@https://vip.97kys.com/vip/?url=",
+    "菜鸟解析@https://jiexi.bm6ig.cn/?url=",
+    "爱解析@http://jx.wfxzzx.cn/?url=",
+    "全网无广解析@https://jx.yswy.top/?url=",
+    "诺讯解析1@http://www.ckmov.com/?url=",
+    "诺讯解析②@https://www.ckmov.com/?url=",
+    "梦城解析@http://api.mcncn.cn/?url=",
+    "新诺讯解析@https://www.nxflv.com/?url=",
+    "我爱解析@http://vip.52jiexi.top/?COLLCC=2964076775&COLLCC=865063344&url=",
+    "52a解析①@http://jx.52a.ink/?url=",
+    "jsr解析@http://jx.isryun.com/?v=",
+    "无名解析@http://69p.top/?url=",
+    "千树解析@http://jx.lengmouwl.xyz/?url=",
+    "8B解析@http://api.8bjx.cn/?url=",
+    "ferr云@http://www.freeget.org/jx.php?url=",
+    "初颜解析@http://jx.wodym.cn/?url=",
+    "Hk解析@http://jx.rdhk.net/?v=",
+    "搜搜库@http://jx.sosoku.cn/jx/?url=",
+    "爱解析@http://jx.wfxzzx.cn/?url=",
+    "AT520@http://at520.cn/jx/?url=",
+    "360dy解析@http://yun.360dy.wang/jx.php?url=",
+    "清风明月@http://fateg.xyz/?url=",
+    "771解析@https://vip.qi71.cn/jiexi.php?url=",
+    "黑云解析@http://jx.daheiyun.com/?url=",
+    "tv920解析@https://api.tv920.com/vip/?url=",
+    "逆天解析@http://nitian9.com/?url=",
+    "羊分享@http://buchi.me/?v=",
+    "超清干货@http://k8aa.com/jx/index.php?url=",
+    "弦易阁@http://jx.hongyishuzhai.com/index.php?url=",
+    "飞鸟云播@http://jx.ledboke.com/?url=",
+    "酷博@http://jx.x-99.cn/api.php?id=",
+    "石头云@http://jiexi.071811.cc/jx.php?url=",
+    "WoCao@https://www.wocao.xyz/index.php?url="
+];
+//需要VIP解析的网站
+function isVip() {
+    var domain = location.href.split('?');
+    return /(\.iqiyi\.com|\.youku\.com|\.le\.com|m\.bilibili.com|m\.miguvideo\.com|\.1905\.com|\.baofeng\.com|\.pptv\.com|\.letv\.com|v\.qq\.com|\.tudou\.com|\.mgtv\.com|\.sohu\.com)/.test(domain[0]);
+}
+
+var s = window.vip;
+var vip = [];
+for (var i = 0; i < s.length; i++) {
+    var k = s[i].split("@");
+    vip.push({
+        name: k[0],
+        url: k[1]
+    });
+}
+
+addStyle(`
+._V_vip_btn {
+  font-size: 18px;
+  width: 42px;
+  height: 42px;
+  bottom: 100px;
+  right: 20px;
+  color: #f8f9fa;
+  background: #40c057;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  text-shadow: 0 0 1px #2f9e44, 0 0 3px #2f9e44, 0 0 4px #2f9e44, 0 0 8px #2f9e44, 0 0 12px #51cf66, 0 0 2px #2f9e44, 0 0 20px #51cf66;
+  position: fixed;
+  z-index: 99999;
+  border-radius: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+._V_vip_btn, ._V_vip_panel li {
+  transition: all .5s;
+}
+
+._V_vip_panel {
+  background: #fff;
+  box-shadow: 0px 1px 10px rgba(0,0,0,0.3);
+  margin: 0;
+  padding: 0 15px;
+  position: fixed;
+  bottom: 120px;
+  right: 72px;
+  z-index: 99999;
+  overflow: scroll;
+  border-radius: 5px;
+  max-height: 80vh;
+}
+
+._V_vip_panel li {
+  margin: 0;
+  font-size: 18px;
+  min-width: 128px;
+  text-align: left;
+  border-bottom: 1px solid #ffe8cc;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 8px 0;
+  letter-spacing: 1px;
+}
+
+._V_vip_title {
+  font-weight: bold;
+  color: #495057;
+}
+
+._V_vip_title:hover{
+  color: red;
+}
+
+._V_vip_url {
+  font-size: 0.85em;
+  color: #868e96;
+}
+
+._V_vip_url:hover {
+  color: red;
+}
+`);
+
+loadVipFunc();
+
+function loadVipFunc() {
+    if (isVip()) {
+        if (document.getElementById('VipButton') != null) {
+            return;
+        }
+        var myBtn = document.createElement('div');
+        var myul = document.createElement('ul');
+        myBtn.id = 'VipButton';
+        myBtn.innerHTML = 'VIP';
+        myBtn.classList.add('_V_vip_btn');
+        document.body.appendChild(myBtn);
+        myul.id = 'VipList';
+        myul.classList.add('_V_vip_panel');
+        myul.style.display = 'none';
+        for (var i = 0; i < vip.length; i++) {
+            var myli = document.createElement('li'); (function(num) {
+                myli.onclick = function() {
+                    window.open(vip[num].url + tryGetRealUrl(location.href), '_blank');
+                };
+            })(i);
+            myli.innerHTML = '<p><span class="_V_vip_title">' + vip[i].name + '</span><br><span class="_V_vip_url">' + vip[i].url.match(/\/\/([^\/]+)\//)[1] + '</span></p>';
+            myul.appendChild(myli);
+        }
+        document.body.appendChild(myul);
+        myBtn.onclick = function() {
+            if (myul.style.display == 'none') {
+                myul.style.display = 'block';
+                myBtn.innerHTML = '➕';
+                myBtn.style.transform = 'rotateZ(45deg)';
+            } else {
+                myul.style.display = 'none';
+                myBtn.innerHTML = 'VIP';
+                myBtn.style.transform = 'rotateZ(0deg)';
+            }
+        };
+    }
+}
+function tryGetRealUrl(url) {
+    var realUrl = url;
+    try {
+        realUrl = getRealUrl(url);
+    } catch(err) {
+        console.log(err);
+    }
+    return realUrl;
+}
+function getYoukuRealUrl(url) {
+    var li = document.getElementsByClassName('hot-row-bottom')[0].children[0];
+    var data = li.getAttribute('data-param');
+    var s = data.split('svid=');
+    if (s.length > 1) {
+        var svid = s[1].split('&')[0];
+        return 'https://v.youku.com/v_show/id_' + svid + '.html';
+    }
+    return url;
+}
+function getRealUrl(url) {
+    var dataurl2 = url;
+    var txurlc = dataurl2.split(':');
+    var txurl = txurlc[1].slice(0, 12);
+    var ykurl = txurlc[1].slice(0, 13);
+    var ykdata = txurlc[1].slice(13);
+    if (ykurl == '//m.youku.com') {
+        return getYoukuRealUrl(url);
+    }
+    if (ykurl == '//m.youku.com') {
+        var txurlc = dataurl2.split(':');
+        var ykurl = txurlc[1].slice(0, 13);
+        var ykdata = txurlc[1].slice(13);
+        dataurl2 = 'http://www.youku.com' + ykdata;
+    } else if (ykurl == '//m.iqiyi.com') {
+        var txurlc = dataurl2.split(':');
+        var ykurl = txurlc[1].slice(0, 13);
+        var ykdata = txurlc[1].slice(13);
+        dataurl2 = 'https://www.iqiyi.com' + ykdata;
+    } else if (txurl == '//m.v.qq.com') {
+        var vid = getParam(dataurl2, 'vid');
+        var cid = getParam(dataurl2, 'cid');
+        var txdata2 = dataurl2.split('?');
+        var str = 'play.html';
+        if (txdata2[0].slice(txdata2[0].length - str.length) == str) {
+            if (cid.length > 1) {
+                dataurl2 = 'https://v.qq.com/x/cover/' + cid + '.html';
+                return dataurl2;
+            } else if (vid.length == 11) {
+                return 'https://v.qq.com/x/page/' + vid + '.html';
+            }
+        }
+        cid = txdata2[0].slice( - 20, -5);
+        if (vid.length == 11) {
+            dataurl2 = 'https://v.qq.com/x/cover/' + cid + '/' + vid + '.html';
+        } else {
+            dataurl2 = 'https://v.qq.com/x/cover/' + cid + '.html';
+        }
+    } else if (ykurl == '//m.le.com/vp') {
+        var leurlc = dataurl2.split('_');
+        var leurl = leurlc[1];
+        dataurl2 = 'http://www.le.com/ptv/vplay/' + leurl;
+    }
+    return dataurl2;
+}
+function getParam(dataurl2, name) {
+    return dataurl2.match(new RegExp('[?&]' + name + '=([^?&]+)', 'i')) ? decodeURIComponent(RegExp.$1) : '';
+}
+function addStyle(styles) {
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    if (css.styleSheet) css.styleSheet.cssText = styles;
+    // Support for IE
+    else css.appendChild(document.createTextNode(styles)); // Support for the rest
+    document.getElementsByTagName('head')[0].appendChild(css);
+}
+})();
+
+
+//==========via-plugin:14==========
+//==========纯净苍穹==========
+(function() {
+/*
+ * @name: 纯净苍穹
+ * @Author: 谷花泰
+ * @version: 1.0
+ * @description: 对常用网站去掉app下载提示
+ * @include: *
+ * @createTime: 2019-10-13 08:46:24
+ * @updateTime: 2019-10-13 14:29:01
+ */
+(function () {
+  /* 执行判断 */
+  const key = encodeURIComponent('谷花泰:纯净苍穹:执行判断');
+  if (window[key]) {
+    return;
+  };
+  window[key] = true;
+
+  class FuckAD {
+    constructor(configs) {
+      this._configs = configs;
+
+      /*
+       * config里的配置解释
+       * {
+       *   正则匹配的域名数组
+       *   sites: ['zhihu.com'],
+       * 
+       *   移除的节点数组
+       *   remove: ['#id'],
+       * 
+       *   display隐藏的节点数组
+       *   displayNone: ['#id'],
+       * 
+       *   visibility隐藏的节点数组
+       *   visibilityHidden: ['#id'],
+       * 
+       *   额外的css
+       *   style: `
+       *   body {
+       *     background-color: #000;
+       *   }
+       *   `,
+       *  
+       *   额外的函数执行
+       *   others() {
+       *     console.log('others: 哈哈');
+       *   }
+       * }
+       *
+       */
+
+      /* 初始化 */
+      this.init();
+    };
+    /*
+     * 初始化
+     */
+    init() {
+      const that = this;
+      /* 所有要移除的节点 */
+      let remove = [];
+      /* 总体style */
+      let style = '';
+      /* 要执行的其它函数集 */
+      let others = [];
+
+      /* 统计 */
+      this._configs.forEach(config => {
+        const canLoad = that.siteInList(config.sites);
+        if (canLoad) {
+          remove = remove.concat(config.remove);
+          style += (config.style || '');
+          style += (that.letSelectorsDisplayNone(config.displayNone));
+          style += (that.letSelectorsVisibilityHidden(config.visibilityHidden));
+          config.others && (others = others.concat(config.others));
+        };
+      });
+
+      /* 添加style */
+      this.addStyle(style);
+      that.removeNodesBySelectors(remove);
+
+      /* 执行others内所有函数 */
+      try {
+        others.forEach(func => {
+          func();
+        });
+      } catch (err) {
+        console.error('via: others function run error', err);
+      };
+
+      /* 监听dom，确保节点移除 */
+      if (remove && remove.length > 0) {
+        this.observe({
+          targetNode: document.documentElement,
+          config: {
+            attributes: false
+          },
+          callback(mutations, observer) {
+            that.removeNodesBySelectors(remove);
+          }
+        })
+      }
+    };
+    /*
+     * 监听dom节点加载函数
+     */
+    observe({ targetNode, config = {}, callback = () => { } }) {
+      if (!targetNode) {
+        return;
+      };
+
+      config = Object.assign({
+        attributes: true,
+        childList: true,
+        subtree: true
+      }, config);
+
+      const observer = new MutationObserver(callback);
+      observer.observe(targetNode, config);
+    };
+    /*
+     * 添加style 
+     */
+    addStyle(style = '') {
+      const styleElm = document.createElement('style');
+      styleElm.innerHTML = style;
+      document.head.appendChild(styleElm);
+    };
+    /*
+     * 选择节点，返回节点数组
+     */
+    selectNodes(selector) {
+      if (!selector) {
+        return [];
+      };
+      const nodes = document.querySelectorAll(selector);
+      return nodes;
+    };
+    /*
+     * 判断网站是否在名单内 
+     */
+    siteInList(sites) {
+      const hostname = window.location.hostname;
+      const result = sites.some(site => {
+        if (hostname.match(site)) {
+          return true;
+        }
+        return false;
+      });
+      return result;
+    };
+    /*
+     * 移除多个节点
+     */
+    removeNodes(nodes = []) {
+      Array.from(nodes, node => {
+        node.parentNode.removeChild(node);
+      });
+    };
+    /*
+     * 根据selector数组移除多个节点
+     */
+    removeNodesBySelectors(selectors = []) {
+      let nodeArr = [];
+      selectors.forEach(selector => {
+        const nodes = this.selectNodes(selector);
+        if (nodes && nodes.length > 0) {
+          nodeArr = nodeArr.concat(Array.from(nodes));
+        };
+      });
+      this.removeNodes(nodeArr);
+    };
+    /*
+     * 根据css选择器生成style
+     */
+    generateStyleBySelectors(selectors = [], customStyle = `{}`) {
+      if (!selectors || selectors.length === 0) {
+        return '';
+      };
+      let style = '';
+      selectors.forEach(selector => {
+        if (selector) {
+          style += `
+          
+          ${selectors} ${customStyle}
+  
+          `;
+        };
+      });
+      return style;
+    };
+    /*
+     * 让数组里的选择器全部 display: none
+     */
+    letSelectorsDisplayNone(selectors = []) {
+      return this.generateStyleBySelectors(selectors, `{
+        display: none!important;
+      }`);
+    };
+    /*
+     * 让数组里的选择器全部 visibility: hidden
+     */
+    letSelectorsVisibilityHidden(selectors = []) {
+      return this.generateStyleBySelectors(selectors, `{
+        visibility: hidden!important;
+      }`);
+    };
+  };
+
+  new FuckAD([
+    {
+      sites: ['bilibili.com'],
+      displayNone: [
+        '.index__openAppBtn__src-commonComponent-topArea-',
+        '.index__container__src-commonComponent-bottomOpenApp-',
+        '.index__openApp__src-videoPage-related2Col-videoItem-',
+        '.index__floatOpenBtn__src-videoPage-floatOpenBtn-',
+        '.index__downLoadBtn__src-videoPage-commentArea-',
+        '.bili-app-link-container',
+        '.open-app-bar',
+        '.btn-ctnr',
+        '.bili-app',
+        '#openAppBtn'
+      ]
+    },
+    {
+      sites: ['zhihu.com'],
+      displayNone: [
+        '.MobileAppHeader-downloadLink',
+        '.ContentItem-more',
+        '.TopstoryItem--advertCard',
+        '.DownloadGuide',
+        '.Profile-followButton',
+        '.OpenInAppButton',
+        '.OpenInApp',
+        '.ViewAllInappButton',
+        '.HotQuestions-bottomButton',
+        '.MHotFeedAd',
+        '.MBannerAd',
+        '.HotQuestions'
+      ],
+      style: `
+        .MobileAppHeader-actions {
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+        }
+      `
+    },
+    {
+      sites: ['www.baidu.com', 'm.baidu.com'],
+      remove: [
+        '#header > div:last-child',
+        '.ec_wise_ad'
+      ],
+      displayNone: [
+        '.blank-frame',
+        '#bottom',
+        '.suggest-hot',
+        '.callicon-wrap',
+        '#navs',
+        '#page-copyright',
+        '[data-module="xcxMulti"]',
+      ],
+      visibilityHidden: [
+        '#userinfo-wrap'
+      ],
+      style: `
+        body {
+          background-color: #fff !important;
+          height: 100vh !important;
+        }
+      `
+    },
+    {
+      sites: ['music.163.com'],
+      displayNone: [
+        '.topfr',
+        '.m-homeft',
+        '.u-ft',
+        '.cmt_more_applink'
+      ],
+      visibilityHidden: [
+        '.m-moreLists'
+      ],
+      style: `
+        .m-scroll_wrapper {
+          bottom: 0 !important;
+        }
+      `
+    },
+    {
+      sites: ['y.qq.com'],
+      displayNone: [
+        '#js_mod_dialog',
+        '.top_box',
+        '.bottom_bar',
+        '.top_bar',
+        '.top_operation_box',
+        '.lyric_action',
+        '.sing',
+        '.btn_download',
+        '.open_qqmusic__btn',
+        '.similar_song',
+        '.related_album',
+        '.related_info',
+        '.recommend_mv',
+        '.mod_lead_flow'
+      ]
+    }
+  ]);
+})();
+})();
+//==========via-plugin:14==========
+
+     
+
+    //小说双击自动滚动功能，白名单设置
+    function openReadScrollMode() {
+        let whiteList = ["jx.hao0606.com"];
+        let inWhiteList = false;
+        for (let i = 0; i < whiteList.length; i++) {
+            if (location.href.indexOf(whiteList[i]) != -1) {
+                inWhiteList = true;
+                break;
+            }
+        }
+        if (!inWhiteList) {
+            eval(function(p, a, c, k, e, r) {
+                e = function(c) {
+                    return (c < a ? '': e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+                };
+                if (!''.replace(/^/, String)) {
+                    while (c--) r[e(c)] = k[c] || e(c);
+                    k = [function(e) {
+                        return r[e]
+                    }];
+                    e = function() {
+                        return '\\w+'
+                    };
+                    c = 1
+                };
+                while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+                return p
+            } ('(5(){6(3.1c(\'L\')){}1b{4 f=3.1a(\'b\');f.19=\'L\';3.7.18(f);13(J,12);5 J(){N();6(9.w.Y(\'X\')>-1){9.t(0,0);i=H;j[h]=M(r,s);h++}}4 g,u,p,s=W;4 h=0;4 i=C;4 j=o V();5 r(){g=3.B.l||3.7.l;9.t(0,++g)};5 F(){i=C;T(4 x=0;x<j.S;x++){R(j[x])};h=0};5 N(){K(2,3.7,5(a,n){i=H;j[h]=M(r,s);h++});4 b=3.P(\'7\')[0];3.m("Q",5(e){u=e.I[0].G});3.m("U",5(e){p=e.I[0].G;4 y=p-u;﻿6(y>=15){9.w="";F()}})};4 k=0;3.m("t",A);5 A(){z=3.B.l||3.7.l;6(z+9.Z.10>3.7.O+11){k=k*1+1;6(k>1&&i){}}}5 K(n,c,d){c.14(\'16\',17);4 n=E(n)<1?1:E(n),8=0,q=0;4 e=5(a){4 b=o v().D();8=(b-q)<1d?8+1:0;q=o v().D();6(8>=n-1){d(a,n);8=0}};c.m(\'1e\',e)}}})();', 62, 77, '|||document|var|function|if|body|count|window||||||||||||scrollTop|addEventListener||new|pooendY|lastTime|pooScrollWin|poomsec|scroll|poostartY|Date|dataOxp|||topin|dddb|documentElement|false|getTime|parseInt|pooScrollWin_Off|clientY|true|changedTouches|okrex|nclickEvent|xxmRead2019|setInterval|scrollAutoClickNextUrl|scrollHeight|getElementsByTagName|touchstart|clearInterval|length|for|touchmove|Array|40|oxp|indexOf|screen|height|110|400|setTimeout|removeEventListener||dblclick|null|appendChild|id|createElement|else|getElementById|300|click'.split('|'), 0, {}))
+        }
+    }
+    //小说双击自动滑动，多次双击可以加速，上滑停止滑动；
+    //如果要关闭此功能，可以在下面一行代码前加上"//"即可
+    openReadScrollMode();
+
+
+})();
