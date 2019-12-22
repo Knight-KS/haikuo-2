6ce95c7472d865583da7ee71c6e8b75ce76e9a67
tianshideyou <chenhua.xu@qq.com>
2019-12-22 11:27:13 +0800

    update global_One.js.

--- global_One.js
+++ global_One.js
@@ -9,7 +9,7 @@ var VERSION = '0.12.19';
 // VIP视频解析 - 解析网址
 var VIP_URLS = `
  腾讯专用 https://vip.66parse.club/?url=
- 爱奇艺专用 https://m2090.com/?url=
+ 爱奇艺专用 https://www.playm3u8.cn/jiexi.php?url=
  h8x http://www.h8jx.com/jiexi.php?url=
     优酷专用 http://www.10yy.com.cn/?url=
       360解析 http://jx.cx77m1.cn/?url= 
